b7cf971c0d172387ff740ebc830d1992cd78589f
ricardo <rgranvilla@gmail.com>
2022-10-21 23:31:55 -0300

    feat(dc-ui): design system initial config

---
+++ shared/dev-commerce-ui/.eslintrc.cjs
@@ -0,0 +1,4 @@
+module.exports = {
+  root: true,
+  extends: ["@dev-commerce/eslint-config-custom"],
+};
---
+++ shared/dev-commerce-ui/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Dev Commerce UI</h1>
+    </div>
+  );
+}
+
+export { App };
