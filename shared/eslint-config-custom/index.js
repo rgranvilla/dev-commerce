149c804164a252eeb188a56cf8bb97662622610c
ricardo <rgranvilla@gmail.com>
2022-10-21 23:28:21 -0300

    feat(eslint): configure eslint

--- shared/eslint-config-custom/index.js
+++ shared/eslint-config-custom/index.js
@@ -1,7 +1,35 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  extends: [
+    '@rocketseat/eslint-config/react',
+    'turbo',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 2020,
+  },
   rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'all',
+        tabWidth: 2,
+        semi: true,
+        singleQuote: true,
+        parser: 'typescript',
+      },
+    ],
   },
 };
