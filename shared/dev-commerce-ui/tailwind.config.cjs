d3ea1513d88024d3819f35721200aed6dd67afe9
ricardo <rgranvilla@gmail.com>
2022-10-24 15:42:56 -0300

    feat(dc-ui): add fonts roboto and baloo 2

--- shared/dev-commerce-ui/tailwind.config.cjs
+++ shared/dev-commerce-ui/tailwind.config.cjs
@@ -8,11 +8,40 @@ module.exports = {
       md: 18,
       lg: 20,
       xl: 24,
-      '2xl': 32,
+      xxl: 32,
+    },
+    colors: {
+      transparent: 'transparent',
+      white: '#FFFFFF',
+      black: '#000000',
+
+      base: {
+        title: '#272221',
+        subtitle: '#403937',
+        text: '#574F4D',
+        label: '#8D8686',
+        hover: '#D7D5D5',
+        button: '#E6E5E5',
+        input: '#EDEDED',
+        card: '#F3F2F2',
+      },
+
+      purple: {
+        50: '#EBE5F9',
+        400: '#8047F8',
+        700: '#4B2995',
+      },
+
+      yellow: {
+        50: '#F1E9C9',
+        400: '#DBAC2C',
+        700: '#C47F17',
+      },
     },
     extend: {
       fontFamily: {
-        sans: 'Work Sans, sans-serif',
+        sans: 'Roboto, sans-serif',
+        baloo: ['"Baloo 2"'],
       },
     },
   },
